649d34b297f7ccfa5cf96dca0ad29f613860469c
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-04-21 19:33:55 +0800

    Update main.php

--- main.php
+++ main.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="icon/qr.png" type="icon">
+    <link rel="shortcut icon" href="icon/qr.png" type="image/x-icon">
     <title>Student Qr Generate</title>
 </head>
 <body>
 
-    <center>
-        <h1>Hello World!</h1>
-    </center>
+    <video autoplay muted loop>
+        <source src="video/bg.mp4" type="video/mp4">
+    </video>
+
+    <div class="qr_generate">
+            <form action="main.php" method="post">
+                <h1>STUDENT GENERATE QR CODE</h1>
+            </form>
+    </div>
     
 </body>
 </html>
