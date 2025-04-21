1770b8920eda88344076d0b5d34376ab034e6120
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-04-21 12:18:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="icon/person.png" type="icon">
     <title>Login</title>
 </head>
 <body>
