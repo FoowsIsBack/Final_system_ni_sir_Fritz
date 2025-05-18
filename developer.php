78261cbb6d5650bd0dbab2cffb920030a6ea61f8
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-05-18 19:33:07 +0800

    Add files via upload

---
+++ developer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="icon/dev.png" type="image/x-icon">
+    <title>Developers</title>
+</head>
+<body>
+    
+    <div class="bg">
+        <video autoplay muted loop>
+            <source src="video/bg.mp4" type="video/mp4">
+        </video>
+    </div>
+
+    <div class="dev_title">
+        <h1 class="h1_title">D E V E L O P E R S</h1>
+    </div>
+
+    <div class="led">
+        <h1 class="leader">LEADER</h1>
+    </div>
+
+    <div class="dave">
+        <a href="https://www.facebook.com/jhinbangcoyo" target="_blank">
+            <img src="dev_icon/dave.jpg" alt="Dave">
+        </a>
+        <h2>Name: Kiryll Dave Bangcoyo</h2>
+        <h2>Section: BSIT - 2B</h2>
+        <a href="main.php">
+            <img src="icon/back.png" alt="back">
+        </a>
+    </div>
+
+
+    <div class="member">
+        <h1 class="leader">MEMBER</h1>
+    </div>
+
+    <div class="brylle">
+        <a href="https://www.facebook.com/johnbrylle.camancho.12" target="_blank">
+            <img src="dev_icon/brylle.jpg" alt="Brylle">
+        </a>
+        <h2>Name: John Brylle Camancho</h2>
+        <h2>Section: BSIT - 2B</h2>
+    </div>
+
+    <div class="member1">
+        <h1 class="leader">MEMBER</h1>
+    </div>
+
+    <div class="lopez">
+        <a href="https://www.facebook.com/kim.joshua.lopez.2025" target="_blank">
+            <img src="dev_icon/lopez.jpg" alt="lopez ">
+        </a>
+        <h2>Name: Kim Joshua Lopez</h2>
+        <h2>Section: BSIT - 2B</h2>
+    </div>
+
+</body>
+</html>
