5495e146f3b297e09f35a94a09886c9a897f8cba
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-05-25 21:37:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -61,7 +61,7 @@
 
             if (mysqli_num_rows($result) === 1) {
                 sleep(3);
-                header("Location: main.php");
+                header("Location: list.php");
                 exit;
             } else {
                 echo("<script>alert('Incorrect username or password!'); window.location.href='login.php';</script>");
