5603f994cab61735e32935eea32a0989b3e03429
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-06-05 20:31:33 +0800

    Update section.php

--- section.php
+++ section.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="icon/dashboard.png" type="image/x-icon">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" href="icon/dashboard.png" type="image/x-icon" />
     <title>All Sections</title>
 </head>
 <body>
 
     <video autoplay muted loop>
-        <source src="video/bg.mp4" type="video/mp4">
+        <source src="video/bg.mp4" type="video/mp4" />
     </video>
 
     <div class="dash_icon">
-        <img src="icon/dashboard.png" alt="icon">
+        <img src="icon/dashboard.png" alt="icon" />
     </div>
 
     <div class="dash_title">
@@ -36,37 +36,37 @@
     <div class="section_info">
         <div class="box1">
             <h2 class="h22">BSIT - 2A</h2>
-            <p class="pp">Total Students = <span class="total_number"><?= ($total_2a) ?></span></p>
+            <p class="pp">Total Students = <span class="total_number"><?= htmlspecialchars($total_2a) ?></span></p>
         </div>
         <br>
 
         <div class="student_sec">
-            <button class="all_student_sec" onclick="window.location.href='2A.php'">All Student</button>
-            <button class="all_student_sec2" onclick="window.location.href='2B.php'">All Student</button>
-            <button class="all_student_sec3" onclick="window.location.href='2C.php'">All Student</button>
+            <button class="all_student_sec" onclick="window.location.href='2A.php'">All Students</button>
+            <button class="all_student_sec2" onclick="window.location.href='2B.php'">All Students</button>
+            <button class="all_student_sec3" onclick="window.location.href='2C.php'">All Students</button>
         </div>
 
         <div class="box2">
             <h2 class="h22">BSIT - 2B</h2>
-            <p class="pp">Total  Students = <span class="total_number"><?= ($total_2b) ?></span></p>
+            <p class="pp">Total Students = <span class="total_number"><?= htmlspecialchars($total_2b) ?></span></p>
         </div>
         <br>
 
         <div class="box3">
             <h2 class="h22">BSIT - 2C</h2>
-            <p class="pp">Total  Students = <span class="total_number"><?= ($total_2c) ?></span></p>
+            <p class="pp">Total Students = <span class="total_number"><?= htmlspecialchars($total_2c) ?></span></p>
         </div>
     </div>
 
     <div class="out2">
         <a href="login.php">
-            <img src="icon/signout.png" alt="logout">
+            <img src="icon/signout.png" alt="logout" />
         </a>
     </div>
 
     <div class="resultqrqr1">
         <button class="r_qr" onclick="window.location.href='dashboard.php'">Dashboard</button>
     </div>
-    
+
 </body>
 </html>
